12ff807620fc8245ef50283fec2aa7b712d21177
samanhoseinpour <samangithoseinpour@gmail.com>
2025-08-10 21:29:24 +0330

    feat: initial config, routes, 404

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export { Banner, Navbar, Footer };
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-export default function Home() {
+const ContactPage = () => {
   return (
     <div className="font-bold grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -8,4 +6,6 @@ export default function Home() {
       </main>
     </div>
   );
-}
+};
+
+export default ContactPage;
