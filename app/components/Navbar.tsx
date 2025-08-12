066f8f466d9416a764e8b21c595b863264f79581
samanhoseinpour <samangithoseinpour@gmail.com>
2025-08-12 12:44:32 +0330

    fix: temporary navbar ui

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,10 +3,29 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="fixed inset-x-0 top-0 z-50">
-      <div className="container mx-auto py-4 bg-black/30 text-white backdrop-blur-3xl rounded-full mt-4 flex justify-around items-center font-semibold">
-        <Link href="/">Logo</Link>
-        <p>Navbar Links</p>
-        <Link href="/contact">Get In Touch With Us</Link>
+      <div className="relative container mx-auto py-4 bg-black/30 text-white backdrop-blur-3xl rounded-full mt-4 flex items-center justify-between font-semibold">
+        <Link href="/" className="px-12">
+          Logo
+        </Link>
+        <div className="absolute left-1/2 transform -translate-x-1/2">
+          <ul className="flex items-center justify-center gap-10">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/solutions">Solutions</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <Link href="/contact" className="px-12">
+          Get In Touch With Us
+        </Link>
       </div>
     </nav>
   );
